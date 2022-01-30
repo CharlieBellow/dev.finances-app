c578d12c7950adb07c3ae08f4cc690fed2d12ded
CharlieBellow <Charliebftm@gmail.com>
2022-01-30 19:38:09 -0300

    add functions

--- scripts.js
+++ scripts.js
@@ -46,6 +46,13 @@ const transactions = [
     description: 'Internet',
     amount: -20000, /* equivale a 500,00 reaistem que colocar o valor sem as vírgulas pq depois ai ser formatado adequadamente */
     date: '23/01/2021',
+  },
+
+  {
+    id: 4,
+    description: 'App',
+    amount: 200000, /* equivale a 500,00 reaistem que colocar o valor sem as vírgulas pq depois ai ser formatado adequadamente */
+    date: '23/01/2021',
   }
 ]
 
@@ -64,3 +71,45 @@ const Transaction = {
 
 // pegar as transações do objeto criado no array transactions e colocar no HTML
 // substituir os dados do HTML com os dados do JS
+const DOM = {
+
+  transactionsContainer: document.querySelector('#data-table tbody'),
+  
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+    DOM.transactionsContainer.appendChild(tr)
+  },
+  
+  innerHTMLTransaction(transaction) {
+    
+    const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+    const amount = Utils.formatCurrency(transaction.amount)
+
+
+
+    // o inner tá montando o HTML
+    const html = `
+      <td class="description">${transaction.description}</td>
+      <td class="${CSSclass}">${transaction.amount}</td>
+      <td class="date">${transaction.date}</td>
+      <td>
+      <img src="./assets/minus.svg" alt="remover transação">
+      </td>
+    `
+    return html
+    /* pra tirar as coisas que tem dentro da função e usar fora dela, tem que colocar o return */
+  }
+}
+
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : ""
+  }
+}
+
+transactions.forEach(function (transaction) {
+  DOM.addTransaction(transaction)
+})
